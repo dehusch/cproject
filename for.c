468510962a0a2570ce2b2db0babec72521a81047
Eberhard Husch <denis.husch@gmail.com>
2021-05-09 22:58:11 -0300

    add for loop in cproject

---
+++ for.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main()
+{
+    int contador;
+    int numero = 1;
+
+    for(contador =1; contador <= 10; contador = contador + 1)
+    {
+        printf("Ola\n");
+
+    }
+
+    return 0;
+}
