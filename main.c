de03df80ec9c244f29b6b01bb50d974f697c5ae9
dehusch <denis.husch@gmail.com>
2021-09-07 22:56:40 -0300

    Learning C

--- main.c
+++ main.c
@@ -7,8 +7,9 @@ int main(int argc, char *argv[])
 {
     puts("Bem vindo");
     puts("\t a linguagem C!");
-    printf("O printf continua ")
-    printf("na mesma linha, e o puts não!")
+    printf("O printf continua ");
+    printf("na mesma linha, e o puts não!\n");
+    printf("Qual\n\tVocê\n\t\tacha\n\t\t\tMelhor?\n");
     return 0;
 }
 // \t insere uma tabulação no texto
