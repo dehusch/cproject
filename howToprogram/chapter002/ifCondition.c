dba529422693e415cc99a3b39c9aab9f30b71de0
dehusch <denis.husch@gmail.com>
2021-09-09 00:14:57 -0300

    update

--- howToprogram/chapter002/ifCondition.c
+++ howToprogram/chapter002/ifCondition.c
@@ -1,2 +1,11 @@
 /*Using if statements, relational operators, and equality operators */
 #include <stdio.h>
+int main() {
+    int num1;
+    int num2;
+
+    printf("Entre com dois numeros inteiros e\n");
+    printf("farei as relações entre eles");
+
+    scanf("%d%d")
+}
