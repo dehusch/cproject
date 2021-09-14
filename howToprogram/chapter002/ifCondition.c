1141958edfb6f7a7cd6032ed1aae356a6b7dca84
dehusch <denis.husch@gmail.com>
2021-09-13 22:31:09 -0300

    C learning project

--- howToprogram/chapter002/ifCondition.c
+++ howToprogram/chapter002/ifCondition.c
@@ -12,19 +12,25 @@ int main() {
     if ( num1 == num2 ){
         printf("%d é igual a %d\n", num1, num2);
     }
+
     if ( num1 != num2 ){
         printf("%d é diferente de %d\n", num1, num2);
     }
+
     if ( num1 < num2 ){
         printf("%d é menor que %d\n", num1, num2);
     }
+
     if ( num1 > num2 ){
-        printf("%d é maior que %d\n", num1, num2);
+        printf("%d e maior que %d\n", num1, num2);
     }
+
     if ( num1 <= num2 ){
-        printf("%d é menor ou igual a %d\n", num1, num2);
+        printf("%d e menor ou igual a %d\n", num1, num2);
     }
+
     if ( num1 >= num2 ){
-        printf("%d é maior ou igual a %d\n", num1, num2);
+        printf("%d e maior ou igual a %d\n", num1, num2);
     }
+    
 }
