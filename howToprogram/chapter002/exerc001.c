78bdf1a04ee1a4f4a804abc275f06df4e40a73ec
dehusch <denis.husch@gmail.com>
2021-09-13 23:35:18 -0300

    Exerc001

---
+++ howToprogram/chapter002/exerc001.c
@@ -0,0 +1,17 @@
+/*Write a single instruction and
+do the follow steps*/
+//Declare a variable
+#include <stdio.h>
+int main() {
+
+    int thisVariable;
+    //ask for a number
+    printf("Enter a int number: "); scanf("%d", &thisVariable);
+    printf("\n");
+    //check if the number is not 7
+    if ( thisVariable != 7 ){
+        printf("%d is not 7", thisVariable);
+    }
+    printf("This is a C Programm\nThis is a C programm\n");
+    printf("This\nis\na\nC\nProgramm\n");
+}
