98722cce9c97a470f62324dd3cdc334a6c4c22bc
dehusch <denis.husch@gmail.com>
2021-09-13 23:56:28 -0300

    Exerc002

---
+++ howToprogram/chapter002/exerc002.c
@@ -0,0 +1,17 @@
+/*Write a calc programm
+with 3 integer numbers*/
+#include<stdio.h>
+
+int main()
+{
+    int number1;
+    int number2;
+    int number3;
+
+    //adress a integer number to variable
+    printf("Type the first value:"); scanf("%d", number1);
+    printf("Type the first value:"); scanf("%d", number2);
+    printf("Type the first value:"); scanf("%d", number3);
+
+    return 0;
+}
