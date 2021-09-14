9dbd3740dd93948d6249a8d0f9a5a216dc80c6ce
dehusch <denis.husch@gmail.com>
2021-09-13 23:43:43 -0300

    Exerc return 0 include

--- howToprogram/chapter002/exerc001.c
+++ howToprogram/chapter002/exerc001.c
@@ -9,10 +9,14 @@ int main() {
     printf("Enter a int number: "); scanf("%d", &thisVariable);
     printf("\n");
     //check if the number is not 7
+
     if ( thisVariable != 7 ){
         printf("%d is not 7\n", thisVariable);
     }
+    
     printf("This is a C Programm\nThis is a C programm\n");
     printf("This\nis\na\nC\nProgramm\n");
     printf("    This\n    is\n    a\n    C\n    Programm\n");
+
+    return 0;
 }
