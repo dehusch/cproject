a6c8a33dad1bfa772cc9ddbe1ac447eccc63e358
dehusch <denis.husch@gmail.com>
2021-09-15 23:14:36 -0300

    Exerc 003

---
+++ howToprogram/chapter002/exerc003.c
@@ -0,0 +1,19 @@
+/*Escreva uma única instrução em C, ou linha única, que
+cumpra os comentários a seguir:
+a) Exiba a mensagem ‘Digite dois números.’
+b) Atribua o produto das variáveis b e c à variável a.
+c) Indique um programa que realize um cálculo de folha
+de pagamento (ou seja, use um texto que ajude
+a documentar um programa).
+d) Informe três valores inteiros usando o teclado e coloque
+esses valores nas variáveis inteiras a, b e c.*/
+#include <stdio.h>
+
+int main(){
+    int varA;
+    int varB;
+    int varC;
+
+    printf()
+
+}
