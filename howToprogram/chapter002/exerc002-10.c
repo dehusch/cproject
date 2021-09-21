a42a4720946e2428ec91d444034a65bc27c74098
dehusch <denis.husch@gmail.com>
2021-09-20 23:03:31 -0300

    Exerc 10

--- howToprogram/chapter002/exerc002-10.c
+++ howToprogram/chapter002/exerc002-10.c
@@ -14,3 +14,22 @@ avaliada da esquerda para a direita.
 e) Os seguintes nomes de variáveis são inválidos: 3g,
 87, 67h2, h22, 2h.
 */
+
+//RESPOSTAS
+
+/*
+2.10 State which of the following are true and which are false. If false, explain your answer.
+a) C operators are evaluated from left to right.
+ANS: False. Some operators are evaluated left to right and others are evaluated from right to left depending on their associativity
+(see Appendix C).
+b) The following are all valid variable names: _under_bar_, m928134, t5, j7, her_sales, his_account_total, a, b,
+c, z, z2.
+ANS: True.
+c) The statement printf("a = 5;"); is a typical example of an assignment statement.
+ANS: False. The statement prints a = 5; on the screen.
+d) A valid arithmetic expression containing no parentheses is evaluated from left to right.
+ANS: False. Multiplication, division, and modulus are all evaluated first from left to right, then addition and subtraction
+are evaluated from left to right.
+e) The following are all invalid variable names: 3g, 87, 67h2, h22, 2h.
+ANS: False. Those beginning with a number are invalid.
+*/
