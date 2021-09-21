ce9167d46682beacf64405df8b0f08f227bea760
dehusch <denis.husch@gmail.com>
2021-09-21 08:25:49 -0300

    Exerc 11 - 12

---
+++ howToprogram/chapter002/exerc002_11.c
@@ -0,0 +1,25 @@
+/*
+2.11 Preencha os espaços de cada sentença:
+a) Que operações aritméticas estão no mesmo nível de
+precedência da multiplicação? ______
+b) Quando os parênteses são aninhados, qual conjunto
+de parênteses é avaliado em primeiro lugar em uma
+expressão aritmética? ______.
+c) Uma posição na memória do computador que pode
+conter diferentes valores em vários momentos
+durante a execução de um programa é chamada
+de ______.
+*/
+
+//RESPOSTAS
+
+/*
+2.11 Fill in the blanks in each of the following:
+a) What arithmetic operations are on the same level of precedence as multiplication? .
+ANS: division, modulus.
+b) When parentheses are nested, which set of parentheses is evaluated first in an arithmetic expression? .
+ANS: The innermost pair of parenthesis.
+c) A location in the computer's memory that may contain different values at various times throughout the execution of a
+program is called a .
+ANS: variable.
+*/
---
+++ howToprogram/chapter002/exerc002_12.c
@@ -0,0 +1,27 @@
+/*
+2.12 O que é exibido quando cada uma das seguintes instruções
+é executada? Se nada for exibido, então responda
+‘Nada’. Considere que x = 2 e y = 3.
+a) printf( “%d”, x );
+b) printf( “%d”, x + x );
+c) printf( “x=” );
+d) printf( “x=%d”, x );
+e) printf( “%d = %d”, x + y, y + x );
+f) z = x + y;
+g) scanf( “%d%d”, &x, &y );
+h) /* printf( “x + y = %d”, x + y ); */
+/*
+i) printf( “\n” );
+*/
+
+//RESPOSTAS
+
+#include <stdio.h>
+
+int main();
+{
+    int a = 2;
+    int b = 2;
+
+    printf("%d", a + a);
+}
