0a1af158148cca697b372cc5b1c0c189ad2ea0a1
dehusch <denis.husch@gmail.com>
2021-09-20 22:52:15 -0300

    Exerc 8 - 10

---
+++ howToprogram/chapter002/exerc002-10.c
@@ -0,0 +1,16 @@
+/*
+2.10 Informe quais das seguintes sentenças são verdadeiras
+e quais são falsas. Explique sua resposta no caso de sentenças
+falsas.
+a) Os operadores em C são avaliados da esquerda para
+a direita.
+b) Os seguintes nomes de variáveis são válidos: _sub_
+barra_, m928134, t5, j7, suas_vendas,
+total_sua_conta, a, b, c, z, z2.
+c) A instrução printf(“a = 5;”); é um exemplo
+típico de uma instrução de atribuição.
+d) Uma expressão aritmética válida sem parênteses é
+avaliada da esquerda para a direita.
+e) Os seguintes nomes de variáveis são inválidos: 3g,
+87, 67h2, h22, 2h.
+*/
---
+++ howToprogram/chapter002/exerc002-8.c
@@ -0,0 +1,30 @@
+/*
+2.8 Preencha os espaços nas seguintes sentenças:
+a) ______ são usados para documentar um programa
+e melhorar sua legibilidade.
+b) A função usada para exibir informações na tela é
+______.
+c) Uma instrução C responsável pela tomada de decisão
+é ______.
+d) Os cálculos normalmente são realizados por instruções
+______.
+e) A função ______ lê valores do teclado.
+*/
+
+//RESPOSTAS
+
+/*
+Fill in the blanks in each of the following:
+a) are used to document a program and improve its readability.
+ANS: comments.
+b) The function used to display information on the screen is .
+ANS: printf.
+6 Introduction to C Programming: Solutions Chapter 2
+© Copyright 1992–2004 by Deitel & Associates, Inc. and Pearson Education Inc. All Rights Reserved.
+c) A C statement that makes a decision is .
+ANS: if.
+d) Calculations are normally performed by statements.
+ANS: assignment.
+e) The function inputs values from the keyboard.
+ANS: scanf.
+*/
