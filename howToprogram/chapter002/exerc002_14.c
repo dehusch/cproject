ae441657a515c9c314fc68d882349522dde9d9d3
dehusch <denis.husch@gmail.com>
2021-09-27 16:37:19 -0300

    Exerc 14 / 15

--- howToprogram/chapter002/exerc002_14.c
+++ howToprogram/chapter002/exerc002_14.c
@@ -1,4 +1,25 @@
 /*
+2.14Dada a equação y = ax3 + 7, quais das seguintes alter-
+nativas são instruções em C corretas para essa equação
+(se houver alguma)?
+Introdução à programação em C
+ 41
+a)
+ y = a * x * x * x + 7;
+b)
+ y = a * x * x * ( x + 7 );
+c)
+ y = ( a * x ) * x * ( x + 7 );
+d)
+ y = ( a * x ) * x * x + 7;
+e)
+ y = a * ( x * x * x ) + 7;
+f)
+ y = a * x * ( x * x + 7 );
+
+*/
+
+/*
  Given the equation y = ax + 7, which of the following, if any, are correct C statements for this equation?
 a) y = a * x * x * x + 7;
 b) y = a * x * x * ( x + 7 );
---
+++ howToprogram/chapter002/exerc003_15.c
@@ -0,0 +1,40 @@
+/*
+2.15 Indique a ordem de avaliação dos operadores em cada
+uma das instruções em C a seguir, e mostre o valor de x
+após a execução de cada instrução.
+a) x = 7 + 3 * 6 / 2 - 1;
+b) x = 2 % 2 + 2 * 2 - 2 / 2;
+c) x = ( 3 * 9 * ( 3 + ( 9 * 3 / ( 3 ) ) ) );
+*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+2.15 State the order of evaluation of the operators in e
+ach of the following C statements and show the value of x 
+after each statement is performed.
+a) x = 7 + 3 * 6 / 2 - 1;
+ANS: * is first, / is second, + is third, and - is 
+fourth. Value of x is 15.
+b) x = 2 % 2 + 2 * 2 - 2 / 2; Resposta: 0 + 4 - 1 = 3
+ANS: % is first, * is second, / is third, + is 
+fourth, - is fifth. Value of x is 3.
+c) x = ( 3 * 9 * ( 3 + ( 9 * 3 / ( 3 ) ) ) );
+ANS:
+ 5
+ 6
+ 4
+ 2
+ 3
+ 1. Value of x is 338.
+*/
