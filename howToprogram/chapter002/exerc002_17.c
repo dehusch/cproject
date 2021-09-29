61e28054c0dd04011e19ed6b5f5acc31deb6fd66
dehusch <denis.husch@gmail.com>
2021-09-28 23:17:21 -0300

    Exerc 17

---
+++ howToprogram/chapter002/exerc002_17.c
@@ -0,0 +1,13 @@
+/*
+Imprimindo valores com printf. Escreva um pro-
+grama que imprima os números de 1 a 4 na mesma li-
+nha. Escreva o programa utilizando os métodos a seguir.
+a)Uma instrução printf sem especificadores de con-
+versão.
+b)Uma instrução printf com quatro especificadores
+de conversão.
+c)Quatro instruções printf.
+
+*/
+
+
