1065dc3b1bc5764e1ba0496cefd1584768f9b84e
dehusch <denis.husch@gmail.com>
2021-09-29 20:15:22 -0300

    Exerc 19

---
+++ howToprogram/chapter002/exerc002_19.c
@@ -0,0 +1,21 @@
+/*
+Aritmética, maior e menor valor. Escreva um pro-
+grama que leia três inteiros diferentes do teclado, depois
+apresente a soma, a média, o produto, o menor e o maior
+desses números. Use apenas a forma de seleção única da
+instrução if que você aprendeu neste capítulo. O diálogo
+na tela deverá aparecer da seguinte forma:
+Digite três inteiros diferentes: 13 27 14
+A soma é 54
+A média é 18
+O produto é 4914
+O menor é 13
+O maior é 27
+*/
+#include <stdio.h>
+
+int main(){
+
+    
+
+}
