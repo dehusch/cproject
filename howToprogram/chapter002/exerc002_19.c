aafea3ca367739e9cba4eb9dc281345852e33291
dehusch <denis.husch@gmail.com>
2021-09-29 20:26:22 -0300

    Exerc 19

--- howToprogram/chapter002/exerc002_19.c
+++ howToprogram/chapter002/exerc002_19.c
@@ -5,6 +5,7 @@ apresente a soma, a média, o produto, o menor e o maior
 desses números. Use apenas a forma de seleção única da
 instrução if que você aprendeu neste capítulo. O diálogo
 na tela deverá aparecer da seguinte forma:
+Exemplo:
 Digite três inteiros diferentes: 13 27 14
 A soma é 54
 A média é 18
@@ -12,10 +13,22 @@ O produto é 4914
 O menor é 13
 O maior é 27
 */
+
 #include <stdio.h>
 
 int main(){
 
-    
+    int x;
+    int y;
+    int z;
+
+    printf("Digite um numero:");
+    scanf("%d", &x);
+    printf("Digite outro numero:");
+    scanf("%d", &y);
+    printf("Digite mais um numero:");
+    scanf("%d", &z);
+
+    printf("%d%d%d", x, y, z);
 
 }
