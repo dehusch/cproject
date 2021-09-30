e394c932c5f9c501a30aee007de93290b68ff978
dehusch <denis.husch@gmail.com>
2021-09-30 19:48:15 -0300

    Exerc 22

---
+++ howToprogram/chapter002/exerc002_22.c
@@ -0,0 +1,14 @@
+/*
+O que o código a seguir imprime?
+printf( “*\n**\n***\n****\n*****\n” );
+*/
+#include <stdio.h>
+
+int main(){
+
+    printf("*\n**\n***\n****\n*****\n");
+
+    return 0;
+    
+}
+
