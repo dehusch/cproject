9b67275a9f20b986fae6842d8479a34b8189f99a
dehusch <denis.husch@gmail.com>
2021-09-30 19:51:19 -0300

    Exerc 23

---
+++ howToprogram/chapter002/exerc002_23.c
@@ -0,0 +1,6 @@
+/*
+Maiores e menores inteiros. Escreva um programa
+que leia cinco inteiros e depois determine e imprima o
+maior e o menor inteiro no grupo. Use apenas as técnicas
+de programação que você aprendeu neste capítulo.
+*/
