c3c82db9c84b1aecb40b077b7be5f7aedca7dfde
dehusch <denis.husch@gmail.com>
2021-09-30 20:32:42 -0300

    Exerc 23

--- howToprogram/chapter002/exerc002_23.c
+++ howToprogram/chapter002/exerc002_23.c
@@ -4,3 +4,70 @@ que leia cinco inteiros e depois determine e imprima o
 maior e o menor inteiro no grupo. Use apenas as técnicas
 de programação que você aprendeu neste capítulo.
 */
+#include <stdio.h>
+
+int main(){
+
+    int a;
+    int b;
+    int c;
+    int d;
+    int e;
+
+    printf("Digite o 1o numero:");
+    scanf("%d", &a);
+
+    printf("Digite o 2o numero:");
+    scanf("%d", &b);
+
+    printf("Digite o 3o numero:");
+    scanf("%d", &c);
+
+    printf("Digite o 4o numero:");
+    scanf("%d", &d);
+
+    printf("Digite o 5o numero:");
+    scanf("%d", &e);
+
+    int menor = a;
+
+    if (b < menor) {
+        menor = b;
+
+    }
+
+    if (c < menor) {
+        menor = c;
+
+    }
+
+    if (d < menor){
+        menor = d;
+    }
+
+    if (e < menor){
+        menor = e;
+    }
+
+    printf("%d é o menor!\n", menor);
+
+    int maior = a;
+
+    if (b > maior){
+        maior = b;
+    }
+
+    if (c > maior){
+        maior = c;
+    }
+
+    if (d > maior){
+        maior = d;
+    }
+
+    if (e > maior){
+        maior = e;
+    }
+
+    printf("%d é o maior!\n", maior);
+}
