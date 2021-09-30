4b188b34d81dcbc83c2fe386d8c3477f5adcf86a
dehusch <denis.husch@gmail.com>
2021-09-29 21:16:21 -0300

    Exerc 21

---
+++ howToprogram/chapter002/exerc002_21.c
@@ -0,0 +1,8 @@
+/*
+2.21o especificador de conversão %f. [Nota: neste capítulo,
+discutimos apenas constantes e variáveis inteiras. No Ca-
+pítulo 3, discutiremos os números em ponto flutuante,
+ou seja, valores que podem ter pontos decimais.]
+Formas com asteriscos. Escreva um programa que
+imprima as seguintes formas com asteriscos.
+*/
