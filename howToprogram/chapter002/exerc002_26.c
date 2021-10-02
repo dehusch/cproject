e6d003489c91c4885da5845934949dc73d57b77d
dehusch <denis.husch@gmail.com>
2021-10-02 20:36:12 -0200

    Exerc 26

---
+++ howToprogram/chapter002/exerc002_26.c
@@ -0,0 +1,13 @@
+/*
+Múltiplos. Escreva um programa que leia dois inteiros,
+determine e imprima se o primeiro for um múltiplo do
+segundo. [Dica: use o operador módulo.]
+*/
+#include <stdio.h>
+
+int main(){
+    
+    int num1;
+    int num2;
+    
+}
