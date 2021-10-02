0290a81f25eaa29862fcafb29eb6bf985e06b0fe
dehusch <denis.husch@gmail.com>
2021-10-02 20:53:36 -0200

    Exerc 26

--- howToprogram/chapter002/exerc002_26.c
+++ howToprogram/chapter002/exerc002_26.c
@@ -9,5 +9,21 @@ int main(){
     
     int num1;
     int num2;
-    
+
+    printf("Digite um numero: ");
+    scanf("%d", &num1);
+    printf("Digite outro numero: ");
+    scanf("%d", &num2);
+
+    if ( num1 % num2 == 0){
+        printf("%d é mutiplo de %d\n", num1, num2);
+        printf("de fator %d\n", num1 / num2);
+
+    }
+
+    if (num1 % num2 != 0){
+        printf("%d não é mutiplo de %d\n", num1, num2);
+
+        return 0;
+    }
 }
