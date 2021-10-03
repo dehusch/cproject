1f0b5e6eda614b5ed098377f357e1c5aa9a3f818
dehusch <denis.husch@gmail.com>
2021-10-03 21:50:20 -0200

    exerc 28

---
+++ howToprogram/chapter002/exerc002_28.c
@@ -0,0 +1,11 @@
+/*
+Faça a distinção entre os termos erro fatal e erro não fa-
+tal. Por que seria preferível experimentar um erro fatal
+em vez de um erro não fatal?
+*/
+
+/*
+Um erro fatal é quando ocorre alguma fala inesperadamente
+ou prematuramente. Um erro não fatal é quando ocorre um
+problema na logica do programa.
+*/
