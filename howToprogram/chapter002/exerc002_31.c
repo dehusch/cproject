3b044354cab05440aabdf554fcb8aa9271bbc15d
dehusch <denis.husch@gmail.com>
2021-10-07 21:08:55 -0200

    Exerc 31

---
+++ howToprogram/chapter002/exerc002_31.c
@@ -0,0 +1,12 @@
+/*
+2.31Tabela de quadrados e cubos. Usando apenas as técni-
+cas que você aprendeu neste capítulo, escreva um programa
+que calcule os quadrados e os cubos dos números 0 a 10, e
+use tabulações para imprimir a seguinte tabela de valores:
+número      quadrado        cubo
+0           0               0
+1           1               1
+2           4               8
+3           9               27
+4           16              64
+*/
