883d644bbe277140a4b5748b62434c90da4184e2
dehusch <denis.husch@gmail.com>
2021-10-10 20:16:15 -0200

    Exerc 32

--- howToprogram/chapter002/exerc002_32.c
+++ howToprogram/chapter002/exerc002_32.c
@@ -3,10 +3,15 @@
 int main(){
 
     int num;
+    int num2;
 
     
     scanf("%d", &num);
-    printf("%d\n", num % 10000);
+    printf("%d\n", num / 10000);
+
+    num2 = num % 10000;
+
+    printf("%d\n", num2 / 1000);
 
     return 0;
 }
