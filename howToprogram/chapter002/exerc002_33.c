242c9f371870339ae294c7578f021cafbee3c06c
dehusch <denis.husch@gmail.com>
2021-10-14 08:50:32 -0300

    Exerc 33

---
+++ howToprogram/chapter002/exerc002_33.c
@@ -0,0 +1,24 @@
+/*
+Calculadora de economias com o transporte
+solidário. Pesquise diversos websites sobre transporte
+solidário com carros de passeio. Crie uma aplicação
+que calcule a sua despesa diária com o automóvel, para
+que você possa estimar quanto dinheiro poderia economizar
+com o transporte solidário, que também tem
+outras vantagens, como reduzir as emissões de carbono
+e os congestionamentos. A aplicação deverá solicitar as
+seguintes informações, e exibir os custos com o trajeto
+diário ao trabalho:
+a) Total de quilômetros dirigidos por dia.
+b) Custo por litro de combustível.
+c) Média de quilômetros por litro.
+d) Preço de estacionamento por dia.
+e) Gastos diários com pedágios.
+*/
+#include <stdio.h>
+
+int main(){
+
+    printf("Teste");
+
+}
