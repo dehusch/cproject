e73e1bcb6c2cc90dba2fac4e1ebe8904bbf1d0b0
dehusch <denis.husch@gmail.com>
2021-10-25 02:30:53 -0300

    Chapter 3 C/C++

--- howToprogram/chapter003/chapter003_intro.c
+++ howToprogram/chapter003/chapter003_intro.c
@@ -201,32 +201,32 @@ A estrutura em pseudocódigo descreve a repetição que ocorre durante uma ida
 
 
 
-    int contador; /* número da nota a digitar em seguida */
-    int nota;/* valor da nota */
+    int cont; /* número da nota a digitar em seguida */
+    int score;/* valor da nota */
     int total;/* soma das notas inseridas pelo usuário */
-    int média;/* média das notas */
+    int med;/* média das notas */
 
     /* fase de inicialização */
 
     total = 0;/* inicializa total */
 
-    contador = 1; /* inicializa contador do loop */
+    cont = 1; /* inicializa contador do loop */
 
     /* fase de processamento */
 
-    while ( contador <= 10 ) {/* loop 10 vezes */
-        printf( “Digite a nota: “ ); /* prompt para inserção */
-        scanf( “%d”, &nota );/* lê a nota do usuário */
-        total = total + nota;/* soma nota ao total */
-        contador = contador + 1;/* incrementa contador */
+    while ( cont <= 10 ) {/* loop 10 vezes */
+        printf( "Digite a nota: " ); /* prompt para inserção */
+        scanf( "%d", &score );/* lê a nota do usuário */
+        total = total + score;/* soma nota ao total */
+        cont = cont + 1;/* incrementa contador */
 
     } /* fim do while */
 
 /* fase de término */
 
-    média = total / 10; /* divisão de inteiros */
+    med = total / 10; /* divisão de inteiros */
 
-    printf( “Média da turma é %d\n”, média ); /* exibe resultado */
+    printf( "Média da turma é %d\n", med ); /* exibe resultado */
 
     return 0; /* indica que programa foi concluído com sucesso */
 
