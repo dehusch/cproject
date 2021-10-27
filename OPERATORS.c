05b2c20edccf83419817de65e8b37ef0c6525508
dehusch <denis.husch@gmail.com>
2021-10-26 23:29:46 -0300

    C Operators

--- OPERATORS.c
+++ OPERATORS.c
@@ -6,6 +6,26 @@ https://www.austincc.edu/akochis/cosc1320/operators.htm
 *******************************************************
 */
 
+#include <stdio.h>
+
+int main(){
+
+    int a;
+    int b;
+    int c;
+    int d;
+    
+    scanf("%d", &a);
+    scanf("%d", &b);
+    
+    c = a % b;
+    d = a / b;
+    printf("Resto da divisao = %d\n", c);
+    printf("Resultado da divisao = %d\n", d);
+
+    return 0;
+
+}
 
 
 /*
