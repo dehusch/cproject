70c3a1b508ea872e6fbc6868e4318b5b833277e8
dehusch <denis.husch@gmail.com>
2022-01-11 00:56:12 -0300

    Vector update

--- vector.c
+++ vector.c
@@ -8,4 +8,18 @@ int main(){
     printf("Altura 2 = %.2f\n", altura[1]);
     printf("Altura 3 = %.2f\n", altura[2]);
 
+    altura[1] = 4; //Pode trocar o valor dos vetores.
+
+    printf("Altura 2 agora é = %.2f\n", altura[1]);
+
+    printf("Digite um novo valor para a altura 3: ");
+    scanf("%f", &altura[2]);
+
+    printf("A altura 3 agora é: %.2f\n", altura[2]);
+
+    float total = (altura[0] + altura[1] + altura[2]);
+    
+    printf("A soma total das alturas é: %.2f\n", total);
+
+    return 0;
 }
