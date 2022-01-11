94e8a8eee0819700e7271c737a0ab28ed275daba
dehusch <denis.husch@gmail.com>
2022-01-11 00:41:34 -0300

    Vector basic

---
+++ vector.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(){
+
+    float altura[3] = {1, 2, 2.3};
+
+    printf("Altura 1 = %.2f\n", altura[0]);
+    printf("Altura 2 = %.2f\n", altura[1]);
+    printf("Altura 3 = %.2f\n", altura[2]);
+
+}
