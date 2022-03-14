8284c34f048413a38cd4752c9cbf752328a44506
dehusch <denis.husch@gmail.com>
2022-03-13 23:08:27 -0300

    Increment Decrement

--- howToprogram/chapter003/chapter003_increment_decrement.c
+++ howToprogram/chapter003/chapter003_increment_decrement.c
@@ -26,3 +26,22 @@ Operador
 --              b--     Usa o valor corrente de b na expressão em que b estiver, e então decrementa b em 1.
 
 */
+
+/* Figura 3.13: fig03_13.c
+Pré-incrementando e pós-incrementando */
+#include <stdio.h>
+/* função main inicia a execução do programa */
+int main( void ){
+    int c; /* define variável */
+    /* demonstra pós-incremento */
+    c = 5; /* atribui 5 a c */
+    printf( "%d\n", c ); /* imprime 5 */
+    printf( "%d\n", c++ ); /* imprime 5 e depois pós-incrementa*/
+    printf( "%d\n\n", c ); /* imprime 6 */
+    /* demonstra pré-incremento */
+    c = 5; /* atribui 5 a c */
+    printf( "%d\n", c ); /* imprime 5 */
+    printf( "%d\n", ++c ); /* pré-incrementa e depois imprime 6 */
+    printf( "%d\n", c ); /* imprime 6 */
+    return 0; /* indica que o programa foi concluído com sucesso */
+} /* fim da função main */
