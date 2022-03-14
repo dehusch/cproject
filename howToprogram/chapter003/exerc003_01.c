75e5dc742289ceda8cced3ea06d05a9a3723c0b7
dehusch <denis.husch@gmail.com>
2022-03-14 20:33:50 -0300

    Exerc003_01

---
+++ howToprogram/chapter003/exerc003_01.c
@@ -0,0 +1,36 @@
+/*
+Preencha os espaços nas seguintes sentenças:
+
+A- O procedimento para resolver um problema em ter-
+mos das ações a serem executadas e da ordem em
+que essas ações devem ser executadas é chamado
+de ______. *Algoritmo
+
+B - Especificar a ordem de execução das instruções pelo
+computador é chamado de ______. *Controle do programa.
+
+Todos os programas podem ser escritos nos termos
+de três tipos de estruturas de controle: ______,
+______ e ______. *Sequencia, seleção e  repetição.
+
+A estrutura de seleção ______ é usada para execu-
+tar uma ação quando uma condição é verdadeira e
+outra ação quando essa condição é falsa. *If
+
+Várias instruções agrupadas com chaves ({ e }) são
+chamadas de ______. *Bloco ou instrução composta.
+
+A estrutura de repetição ______ especifica que
+uma instrução ou grupo de instruções deve ser exe-
+cutada repetidamente enquanto alguma condição
+permanecer verdadeira. *While
+
+A repetição de um conjunto de instruções por um
+número específico de vezes é chamada de repetição
+______. *Controlada
+
+Quando não se sabe com antecedência quantas vezes
+um conjunto de instruções será repetido, um valor de
+______ pode ser usado para encerrar a repetição. *Sentinela
+
+*/
