7c5a04b6293423e276140dbad16abf523b5f4542
dehusch <denis.husch@gmail.com>
2022-03-16 21:07:21 -0300

    Exerc 003_04

---
+++ howToprogram/chapter003/exerc003_04.c
@@ -0,0 +1,12 @@
+/*
+Escreva uma instrução em C que possibilite realizar as
+seguintes tarefas:
+a) declarar variáveis soma e x para que sejam do tipo int.
+b) inicializar variável x em 1.
+c) inicializar variável soma em 0.
+d) somar variável x à variável soma e atribuir o resulta-
+do à variável soma.
+e)imprimir 'A soma é:' seguido pelo valor da variá­vel soma.
+*/
+#include <stdio.h>
+
