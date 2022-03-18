f8c98abf97985980c0c311e73f21b621677cc0f3
dehusch <denis.husch@gmail.com>
2022-03-18 19:36:48 -0300

    Exerc 003 07

---
+++ howToprogram/chapter003/exerc003_07.c
@@ -0,0 +1,14 @@
+/*
+3.7
+Escreva instruções em C que:
+a) leiam a variável inteira x com scanf.
+b) leiam a variável inteira y com scanf.
+c) inicializem a variável inteira i em 1.
+d) inicializem a variável inteira potência em 1.
+e) multipliquem a variável potência por x e atribua o
+resultado à potência.
+f) incrementem a variável i em 1.
+g) testem i para verificar se ele é maior ou igual a y na
+condição de uma estrutura while.
+h) exibam a variável inteira potência com printf.
+*/
