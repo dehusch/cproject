ce2c4fce45d69b4c84204a9ff9bf5b1070699a49
dehusch <denis.husch@gmail.com>
2022-03-19 12:59:34 -0300

    Exerc 003 07

--- howToprogram/chapter003/exerc003_07.c
+++ howToprogram/chapter003/exerc003_07.c
@@ -23,6 +23,33 @@ int main(){
     printf("Digite o Valor de Y: ");
     scanf("%d", &y);
     printf("Y = %d\n", y);
+    i = 4;
+    printf("i = %d\n", i);
+    potencia = 1;
+    printf("potencia = %d\n", potencia);
+    i++;
+    printf("i incrementado é = %d\n", i);
+    while(i >= y){
+        printf("O valor de i é: %d\n", i);
+        printf("Digite o valor de Y: ");
+        scanf("%d", &y);
+        if(y <= i){
+            printf("Y ainda é menor ou igual a i\n");
+        }
+        
+    }
     
     return 0;
-}
+}   
+
+/*
+a) scanf( “%d”, &x );
+b) scanf( “%d”, &y );
+c) i = 1;
+d) potência = 1;
+e) potência *= x;
+f) i++;
+g) if ( i <= y )
+h) printf(
+ “%d”, potência );
+ */
