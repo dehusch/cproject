6a6a5b4f46127dc5d48cdd6456c10645d16ba26d
dehusch <denis.husch@gmail.com>
2022-03-20 18:29:41 -0300

    Exerc 003 09

---
+++ howToprogram/chapter003/exerc003_09.c
@@ -0,0 +1,16 @@
+/*
+Identifique e corrija os erros em cada um dos itens:
+
+a) while ( c <= 5 ) {
+    produto *= c;
+    ++c;
+b) scanf( “%.4f”, &valor );
+c) if ( sexo == 1 )
+    printf( “Mulher\n” );
+   se não
+    printf( “Homem\n” );
+ */
+
+ //a) faltou }
+ //b) retirar .4
+ //c) faltou else e {}
