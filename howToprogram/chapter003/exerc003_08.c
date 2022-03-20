7a0be568a30df045f7a9978c853f7c8440a509e2
dehusch <denis.husch@gmail.com>
2022-03-20 17:31:28 -0300

    Exerc 003 08

--- howToprogram/chapter003/exerc003_08.c
+++ howToprogram/chapter003/exerc003_08.c
@@ -5,42 +5,15 @@ deverá ter uma estrutura de controle de repetição while.
 */
 #include<stdio.h>
 
-int x, y, i, potencia;
+int x, y, potencia;
+
+x = 3;
+y = 2;
 
 int main(){
-    printf("Digite o Valor de X: ");
-    scanf("%d", &x);
-    printf("X = %d\n", x);
-    printf("Digite o Valor de Y: ");
-    scanf("%d", &y);
-    printf("Y = %d\n", y);
-    i = 4;
-    printf("i = %d\n", i);
-    potencia = 1;
-    printf("potencia = %d\n", potencia);
-    i++;
-    printf("i incrementado é = %d\n", i);
-    while(i >= y){
-        printf("O valor de i é: %d\n", i);
-        printf("Digite o valor de Y: ");
-        scanf("%d", &y);
-        if(y <= i){
-            printf("Y ainda é menor ou igual a i\n");
-        }
-        
+    potencia = x*(x*y);
+    printf("%d", potencia);
     }
     
     return 0;
 }   
-
-/*
-a) scanf( “%d”, &x );
-b) scanf( “%d”, &y );
-c) i = 1;
-d) potência = 1;
-e) potência *= x;
-f) i++;
-g) if ( i <= y )
-h) printf(
- “%d”, potência );
- */
