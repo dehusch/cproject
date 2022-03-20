c10c82d524651b54c694910ee45fd1f5da521af8
dehusch <denis.husch@gmail.com>
2022-03-20 18:34:31 -0300

    Exerc 003 10

---
+++ howToprogram/chapter003/exerc003_10.c
@@ -0,0 +1,9 @@
+/*
+O que está errado na estrutura de repetição while a seguir 
+(considere z com valor 100), que supõe-se calcular a soma 
+dos inteiros de 100 para 1?
+
+while (z >= 0)
+soma += z;
+*/
+// R: Se z = 100, entrara em um loop infinito.
