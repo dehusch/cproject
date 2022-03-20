d6de633b00d156ba624084ff48cdfbd52f34e0db
dehusch <denis.husch@gmail.com>
2022-03-20 18:03:45 -0300

    Exerc 003 08

--- howToprogram/chapter003/exerc003_08.c
+++ howToprogram/chapter003/exerc003_08.c
@@ -3,17 +3,20 @@ Escreva um programa em C que use as instruções do Exer-
 cício 3.7 para calcular x elevado à potência y. O programa
 deverá ter uma estrutura de controle de repetição while.
 */
-#include<stdio.h>
+#include <stdio.h>
+int main( void ){
+    int x, y, i, potência; /* declara variáveis */
+    i = 1; /* inicializa i */
+    potência = 1; /* inicializa potência */
 
-int x, y, potencia;
+    scanf( "%d", &x ); /* lê do usuário o valor para x */
 
-x = 3;
-y = 2;
-
-int main(){
-    potencia = x*(x*y);
-    printf("%d", potencia);
-    }
+    scanf( "%d", &y ); /* lê do usuário o valor para y */
     
+    while ( i <= y ) { /* loop while i é menor ou igual a y */
+        potência *= x; /* multiplica potência por x */
+        ++i; /* incrementa i */
+    } /* fim do while */
+    printf( "%d", potência ); /* exibe potência */
     return 0;
-}   
+} /* fim da função main */
