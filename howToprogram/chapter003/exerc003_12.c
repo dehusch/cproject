6bf4eddf3eb94c7c21c993f0820cae2b74f8e113
dehusch <denis.husch@gmail.com>
2022-03-23 22:36:15 -0300

    Exerc 003 12

---
+++ howToprogram/chapter003/exerc003_12.c
@@ -0,0 +1,26 @@
+/*
+a)  A solução para qualquer problema envolve realiizar 
+uma série de ações em um(a) ______ específico(a).
+b)  Um sinônimo para procedimento é ______.
+c)  Uma variável que acumula a soma de vários núme-
+ros é um(a) ______.
+d)  A definição de certas variáveis com valores espe-
+cíficos no início de um programa é chamada de
+______.
+e)  Um valor especial usado para indicar o 'fim da
+entrada de dados' é chamado de valor ______,
+______, ______ ou ______.
+f)  Um(a) ______ é uma representação gráfica de um
+algoritmo.
+g)  Em um fluxograma, a ordem em que as etapas de-
+vem ser realizadas é indicada por símbolos ______.
+h)  O símbolo de finalização indica o(a) ______ e
+______ de cada algoritmo.
+i)  Retângulos correspondem a cálculos que normal-
+mente são realizados por instruções e operações
+de entrada/saída que normalmente são realizadas
+por chamadas às funções ______ e ______ da
+biblioteca-padrão.
+j)  O item escrito dentro de um símbolo de decisão é
+chamado de ______.
+*/
