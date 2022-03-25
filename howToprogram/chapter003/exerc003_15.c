e555f4b200f64b206457666b379f1ecd7fdd6b15
dehusch <denis.husch@gmail.com>
2022-03-24 23:01:44 -0300

    Exerc 003 15

---
+++ howToprogram/chapter003/exerc003_15.c
@@ -0,0 +1,37 @@
+/*
+Formule um algoritmo em pseudocódigo para cada um
+dos seguintes comandos:
+    a) Obter dois números a partir do teclado, calcular sua
+soma e exibir o resultado.
+    b) Obter dois números a partir do teclado, determinar
+e exibir qual deles (se houver algum) é o maior dos
+dois números.
+    c)Obter uma série de números positivos a partir do te-
+clado, determinar e exibir sua soma. Suponha que o
+usuário digite um valor de sentinela −1 para indicar
+o ‘fim da entrada de dados’.
+*/
+/*A) R:
+    Input the first number
+    Input the second number
+    Add the two numbers
+    Output the sum
+
+  B) R:
+    Input the first number from the keyboard
+    Input the second number from the keyboard
+    If the first number is greater than the second number
+        print it
+    Else if the second number is greater than the first number
+        print it
+    Else
+        print a message stating that the numbers are equal
+
+  C) R:
+    Input a value from the keyboard
+    While the input value is not equal to -1
+        add the number to the running total
+        input the next number
+    Print the sum
+
+*/
