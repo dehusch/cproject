cb7867da2d3300ad951180fbd73a575a5855302b
dehusch <denis.husch@gmail.com>
2022-03-24 22:02:35 -0300

    while multiple table

--- while.c
+++ while.c
@@ -4,18 +4,23 @@ int main()
 {
     int contador = 1;
     int x = 0, y = 0, total = 0;
+
     while (contador <=5)
     {
         printf("%i\n", contador);
         contador = contador + 1; //outra forma de representar essa situação é "++contador;"
     }
 
-    while (x <= 10){
+
+    //Multiple table
+    printf("Set the number for multiple table: ");
+    scanf("%d", &x);
+
+    while (y <= 10){
         total = x * y;
-        printf("%d x %d = %d", x, y, total);
-        x++;
+        printf("%d x %d = %d\n", x, y, total);
         y++;
-        
+
     }
 
     return 0;
