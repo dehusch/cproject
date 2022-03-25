f998010364fb88a3cdc01b8a8b6d194000668dd6
dehusch <denis.husch@gmail.com>
2022-03-24 22:34:44 -0300

    Exerc 003 14

---
+++ howToprogram/chapter003/exerc003_14.c
@@ -0,0 +1,19 @@
+/*
+Escreva uma única instrução em pseudocódigo que indi-
+que cada um dos seguintes comandos:
+    a)Apresente a mensagem 'Digite dois números'.
+    b)Atribua a soma das variáveis x, y e z à variável p.
+    c)A seguinte condição deve ser testada em uma estru-
+tura de seleção if...else: o valor atual da variável
+m é maior que o dobro do valor atual da variável v.
+    d)Obtenha valores para as variáveis s, r e t pelo te-
+clado.
+*/
+// A) print "enter two numbers"
+// B) p = x+y+z
+/* C)   if m is greater than twice v
+            do this
+        else
+            do this
+*/
+// D) imput s, input r, input t
