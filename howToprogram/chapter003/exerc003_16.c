df147811622017fc031ee341fe5cd5bf97db0889
dehusch <denis.husch@gmail.com>
2022-03-28 23:53:57 -0300

    exerc16

---
+++ howToprogram/chapter003/exerc003_16.c
@@ -0,0 +1,24 @@
+/*
+Indique quais das sentenças seguintes são verdadeiras
+e quais são falsas. Caso haja uma afirmação falsa, explique.
+    a) A experiência tem mostrado que a parte mais difícil
+    da solução de um programa em um computador é a
+    produção de um programa em C funcional.
+    Falso, a parte mais dificil é solucionar o algoritimo corretamente
+
+    b) Um valor de sentinela deve ser um valor que não possa
+    ser confundido com um valor de dados legítimo.
+    Verdadeiro
+
+    c) Linhas de fluxo indicam as ações a serem realizadas.
+    Falso, elas indicam o sentido das ações
+
+    d) Condições escritas dentro de símbolos de decisão
+    sempre contêm operadores aritméticos (ou seja,
+    +, -, *, / e %).
+    Falso, simbolos de maior ou menor tambem podem ser utilizado
+
+    e)Nos refinamentos sucessivos top-down, cada refinamento
+    é uma representação completa do algoritmo.
+    Verdadeiro
+*/
