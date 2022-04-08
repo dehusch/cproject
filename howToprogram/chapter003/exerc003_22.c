1309887be5666e8172580216bc724f066712a94b
dehusch <denis.husch@gmail.com>
2022-04-07 22:27:59 -0300

    ex22

---
+++ howToprogram/chapter003/exerc003_22.c
@@ -0,0 +1,12 @@
+/*
+Pré-incrementando versus pós-incrementando.
+Escreva um programa que demonstre a diferença entre
+pré-decrementar e pós-decrementar usando o operador
+de decremento --.
+*/
+#include<stdio.h>
+
+int main(){
+    printf("")
+    
+}
