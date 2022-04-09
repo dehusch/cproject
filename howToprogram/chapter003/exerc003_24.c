3be94e4a3242a27544a861ef8c538834d23c98b1
dehusch <denis.husch@gmail.com>
2022-04-08 21:18:50 -0300

    ex24

---
+++ howToprogram/chapter003/exerc003_24.c
@@ -0,0 +1,23 @@
+/*
+Ache o número maior. O processo de achar o número
+maior (ou seja, o máximo de um grupo de números)
+é usado com frequência nas aplicações de computador.
+Por exemplo, um programa que determina o vencedor
+de uma disputa de vendas lerá o número de unidades
+vendidas por vendedor. O vendedor que tiver vendido
+mais unidades vence a disputa. Escreva um programa
+em pseudocódigo e depois um programa que leia uma
+série de 10 números, determine e imprima o maior dos
+números. 
+
+[Dica: seu programa deverá usar três variáveis da seguinte forma]:
+
+contador: Um contador para contar até 10 (ou seja,
+    registrar quantos números foram informados
+    e determinar quando os 10 números
+    foram processados)
+
+número: O número da entrada atual no programa
+
+maior: O maior número achado até o momento
+*/
