6d05337500a9712e984a59b7d4426aa446eb7245
dehusch <denis.husch@gmail.com>
2022-04-12 22:29:17 -0300

    ex24

--- howToprogram/chapter003/exerc003_24.c
+++ howToprogram/chapter003/exerc003_24.c
@@ -26,10 +26,18 @@ maior: O maior número achado até o momento
 int main(int argc, char const *argv[])
 {
     int numero;
+    int maior = 0;
     for (int i = 0; i < 10; i++)
     {
         printf("Digite um numero: ");
         scanf("%d", &numero);
+
+        if (maior > numero)
+        {
+            printf("%d", numero);
+        }
+
+        printf("Numero: %d, Maior: %d", numero, maior);
     }
     
     return 0;
