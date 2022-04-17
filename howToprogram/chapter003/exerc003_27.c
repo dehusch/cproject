9492e82696b3d3d93c88dc3655387328f6776363
dehusch <denis.husch@gmail.com>
2022-04-17 15:05:18 -0300

    ex27

--- howToprogram/chapter003/exerc003_27.c
+++ howToprogram/chapter003/exerc003_27.c
@@ -10,23 +10,30 @@ int main(int argc, char const *argv[])
 {
     int numero;
     int maior = 0;
-    
+    int segmaior = 0;
     printf("Digite um numero: ");
-    scanf("%d", &maior);
+    scanf("%d", &segmaior);
     
     for (int i = 0; i < 9; i++)
     {
         printf("Digite mais um numero: ");
         scanf("%d", &numero);
 
-        if (numero > maior)
+        if (numero > segmaior)
         {
             maior = numero;
+        }
+        else {
+            if (numero > maior){
+                maior = numero;
+            }
+            
         }
         printf("Numero: %d, Maior: %d\n", numero, maior);
     }
     
-    printf("O maior numero e %d", maior);
+    printf("O maior numero e %d\n", maior);
+    printf("O segundo maior e %d\n", segmaior);
 
     return 0;
 }
