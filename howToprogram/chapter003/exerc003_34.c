7d2c8df38e56eff7390944c775477e7fa294b85e
dehusch <denis.husch@gmail.com>
2022-04-26 21:08:31 -0300

    ex34

--- howToprogram/chapter003/exerc003_34.c
+++ howToprogram/chapter003/exerc003_34.c
@@ -20,34 +20,35 @@ int main(int argc, char const *argv[])
     printf( "Digite o numero de lado: " );
     scanf( "%d", &lado );
     
-    tamanho = lado; /* set tamanho counter to length of lado */
+    tamanho = lado; //define o tamanho igual o lado
     
-    /* loop lado number of times */
     while ( lado > 0 ) {
-        posColuna = tamanho; /* set row counter to length of tamanho */
-        /* loop posColuna number of times */
+        posColuna = tamanho;
         while ( posColuna > 0 ) {
-            /* if lado or row counter is 1 or tamanho print an '*' */
+            //Se Tamanho for = ao lado, imprima "*"
             if ( tamanho == lado ) {
             printf( "*" );
-            }   /* end if */
+            }
+            //Se lado == 1, imprima "*"
             else if ( lado == 1 ) {
             printf( "*" );
-            }/* end else if */
+            }
+            //Se posColuna for = 1, imprima "*"
             else if ( posColuna == 1 ) {
             printf( "*" );
-            } /* end else if */
+            }
+            //Se posColuna = tamanho, imprima "*"
             else if ( posColuna == tamanho ) {
             printf( "*" );
-            } /* end else if */
-            else { /* otherwise, print a space */
+            }
+            //Ou então, imprima " "
+            else {
             printf( " " );
-            } /* end else */
-            --posColuna; /* decrement row counter */
-        } /* end inner while */
-        printf( "\n" ); /* new line for next row */
-        --lado; /* decrement lado counter */
-    } /* end outer while */
-    //Testing git fetch
+            }
+            --posColuna; //decrementa o posColuna
+        }
+        printf( "\n" );
+        --lado; //decrementa o lado
+    }
     return 0;
 }
