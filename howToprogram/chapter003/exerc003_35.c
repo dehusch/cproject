a3c519eab2556b892d35b912708c4f0d144b251d
dehusch <denis.husch@gmail.com>
2022-04-28 23:01:18 -0300

    Ex35

---
+++ howToprogram/chapter003/exerc003_35.c
@@ -0,0 +1,25 @@
+/*
+3.35 Testador de palíndromo. Um palíndromo é um número,
+ou uma frase textual, que pode ser lido da mesma
+forma da esquerda para a direita e vice-versa. Por
+exemplo, cada um dos seguintes inteiros de cinco dígitos
+é um palíndromo: 12321, 55555, 45554 e 11611. Escreva
+um programa que leia um inteiro de cinco dígitos e
+determine se ele é ou não um palíndromo. [Dica: use os
+operadores de divisão e módulo para separar o número
+em seus dígitos individuais.]
+*/
+#include<stdio.h>
+
+int main(int argc, char const *argv[])
+{
+    int num;
+
+
+    printf("Digite 5 numeros: ");
+    scanf("%d", &num);
+
+    
+
+    return 0;
+}
