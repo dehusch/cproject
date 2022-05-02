bc4053c66c52a3a4f7bdb816f8176405740e08fd
dehusch <denis.husch@gmail.com>
2022-05-01 21:53:01 -0300

    Ex45

---
+++ howToprogram/chapter003/exerc003_45.c
@@ -0,0 +1,16 @@
+/*Lados de um triângulo retângulo. Escreva um programa
+que leia três inteiros diferentes de zero e determine
+e imprima se eles poderiam ser os lados de um
+triângulo retângulo.
+*/
+#include<stdio.h>
+
+int main(int argc, char const *argv[])
+{
+    int s1;
+    int s2;
+    int s3;
+
+    
+    return 0;
+}
