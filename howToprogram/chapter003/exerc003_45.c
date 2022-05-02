597fb99bdb9a1a7ee18d5049e5fd5c6a2e3a032a
dehusch <denis.husch@gmail.com>
2022-05-01 22:13:17 -0300

    Ex45

--- howToprogram/chapter003/exerc003_45.c
+++ howToprogram/chapter003/exerc003_45.c
@@ -11,6 +11,27 @@ int main(int argc, char const *argv[])
     int s2;
     int s3;
 
+    printf("Input the first value of rectangle triangle: ");
+    scanf("%d", &s1);
+    printf("Input the second value: ");
+    scanf("%d", &s2);
+    printf("Input the third value: ");
+    scanf("%d", &s3);
+
+
+    /*
+    if a*a + b*b = c*c
+    */
+    
+    if (s1*s1+s2*s2 == s3*s3)
+    {
+        printf("It's a triangule rectangle");
+    }
+    else
+    {
+        printf("It's not a Triangule rectangle");
+    }
+    
     
     return 0;
 }
