96de725bc533c5a8a630938cea841af90cde7a68
dehusch <denis.husch@gmail.com>
2022-05-03 15:05:12 -0300

    Ex46

---
+++ howToprogram/chapter003/exerc003_46.c
@@ -0,0 +1,28 @@
+/*
+3.46 Fatorial. O fatorial de um inteiro não negativo n é escrito
+como n! (pronuncia-se ‘n fatorial’) e é definido da
+seguinte forma:
+
+n! = n · (n - 1) · (n - 2) · … · 1 (para valores de n maiores ou iguais a 1)
+e
+n! = 1 (para n = 0).
+Por exemplo, 5! = 5 · 4 · 3 · 2 · 1, que é 120.
+a) Escreva um programa que leia um inteiro não negativo
+e calcule e imprima seu fatorial.
+b) Escreva um programa que calcule o valor da constante
+matemática e usando a fórmula:
+e=1+ + + +
+1
+1
+1
+2
+1
+! ! 3!
+..
+c) Escreva um programa que calcule o valor de ex
+usando a fórmula
+e
+x x x x
+! ! !
+=1+ + + +..
+*/
