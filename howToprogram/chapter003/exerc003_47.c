a7a097d13e0ccca712f4b88231abf56e83e16dc6
dehusch <denis.husch@gmail.com>
2022-06-26 18:51:16 -0300

    Ex 47

---
+++ howToprogram/chapter003/exerc003_47.c
@@ -0,0 +1,15 @@
+/*
+3.47diferença
+Calculadora de crescimento populacional mun-
+dial. Use a Web para determinar a população mundial
+atual e sua taxa de crescimento anual. Escreva uma
+aplicação que leia esses valores, depois apresente a po-
+pulação mundial estimada após um, dois, três, quatro e
+cinco anos.
+
+Calculo = Nasc - Mortes + Saldo Migratorio / populacão total
+Resultado em %
+
+Pop atual = 8 Bilhões
+Taxa de crescimento = 1%
+*/
