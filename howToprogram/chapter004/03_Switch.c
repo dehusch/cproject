0cabc54658763028b74d7cf1f058141ff0bc5c09
dehusch <denis.husch@gmail.com>
2022-09-03 23:32:06 -0300

    Switch

--- howToprogram/chapter004/03_Switch.c
+++ howToprogram/chapter004/03_Switch.c
@@ -1,1 +1,12 @@
 #include<stdio.h>
+//Contando notas de letras
+int main(){
+
+    int nota;
+    int aCont = 0;
+    int bCont = 0;
+    int cCont = 0;
+    int dCont = 0;
+    int fCont = 0;
+    
+}
