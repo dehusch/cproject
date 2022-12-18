1c5d820948b6af5e519f77c64ee0f55b19e296fe
dehusch <denis.husch@gmail.com>
2022-12-18 18:22:22 -0300

    Poem

---
+++ Intro_to_C_UCSC/poem.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+int main(void)
+{   
+    printf("My life was going worng\n");
+    printf("Before i know C\n");
+    printf("Now my life it's fine\n");
+    printf("Because I'm crying every night and can't understand nothing at all!\n");
+    return 0;
+}    
