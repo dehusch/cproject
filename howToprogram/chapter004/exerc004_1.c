2e260ebadea655843a4928f04204f5f154dea430
dehusch <denis.husch@gmail.com>
2022-12-31 01:11:20 -0300

    Exerc 4.1

---
+++ howToprogram/chapter004/exerc004_1.c
@@ -0,0 +1,28 @@
+
+/*
+4.1 Preencha os espaços nas sentenças.
+
+a) A repetição controlada por contador também é conhecida
+como repetição ___definida___, pois sabemos
+com antecedência quantas vezes o loop será executado.
+
+b) A repetição controlada por sentinela também é conhecida
+como repetição ___indefinida___, pois não sabemos
+com antecedência quantas vezes o loop será executado.
+
+c) Na repetição controlada por contador, um(a)
+__variável de controle ou contador__ é usado(a) para contar o número de vezes
+que um grupo de instruções deve ser repetido.
+
+d) O comando ___continue___, quando executado em uma
+estrutura de repetição, faz com que a próxima iteração
+do loop seja realizada imediatamente.
+
+e) O comando ___break___, quando executado em uma
+estrutura de repetição ou em um switch, causa
+uma saída imediata da estrutura.
+
+f) A ___estrutura de seleção switch___ é usada para testar uma variável ou expressão
+em particular para cada um dos valores inteiros
+constantes que ela pode assumir.
+*/
