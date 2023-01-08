6895978c6e2d78a8ce5e1bc40ce2f0109a7df927
dehusch <denis.husch@gmail.com>
2023-01-08 00:02:06 -0300

    Exerc 2

---
+++ howToprogram/chapter004/exerc004_2.c
@@ -0,0 +1,19 @@
+/*
+Indique se as seguintes sentenças são verdadeiras
+ou falsas. Explique sua resposta no caso de haver
+sentenças falsas.
+
+a) O caso default é obrigatório na estrutura de seleção
+switch. Falso
+
+b) O comando break é obrigatório no caso default
+de uma estrutura de seleção switch. Falso
+
+c) A expressão (x > y && a < b) é verdadeira se x
+> y for verdadeiro ou a < b for verdadeiro. Falso
+
+d) Uma expressão contendo o operador || é verdadeira
+se um ou ambos os seus operandos forem verdadeiros.
+Verdadeiro
+
+*/
