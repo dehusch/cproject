4f88097e9ee2596a23cde1d2859bb4e7df4fde1e
dehusch <denis.husch@gmail.com>
2023-01-13 21:14:30 -0300

    Solved Cap04 Exerc 6 How to Program book

---
+++ howToprogram/chapter004/exerc004_6.c
@@ -0,0 +1,34 @@
+/*
+4.6 Indique quais valores da variável de controle x são impressos
+por cada uma das seguintes estruturas for:
+
+
+a) for ( x = 2; x <= 13; x += 2 ) {
+printf( “%d\n”, x );
+}
+R: 2, 4, 6, 8, 10, 12.
+
+
+b) for ( x = 5; x <= 22; x += 7 ) {
+printf( “%d\n”, x );
+}
+R: 5, 12, 19.
+
+
+c) for ( x = 3; x <= 15; x += 3 ) {
+printf( “%d\n”, x );
+}
+R: 3, 6, 9, 12, 15
+
+
+d) for ( x = 1; x <= 5; x += 7 ) {
+printf( “%d\n”, x );
+}
+R: 1
+
+e) for ( x = 12; x >= 2; x -= 3 ) {
+printf( “%d\n”, x );
+}
+R: 12, 9, 6, 3
+
+*/
