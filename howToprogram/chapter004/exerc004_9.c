5748730edba42987e0821f43f8204b614c594e7b
dehusch <denis.husch@gmail.com>
2023-01-15 19:51:20 -0300

    Solved Cap04 Exerc 9 How to Program book

---
+++ howToprogram/chapter004/exerc004_9.c
@@ -0,0 +1,9 @@
+/*Soma de uma sequência de inteiros. Escreva um
+programa que some uma sequência de inteiros. Considere
+que o primeiro inteiro lido com scanf especifique o
+número de valores restantes a serem inseridos. Seu programa
+deve ler apenas um valor toda vez que scanf for
+executado. Uma sequência de entrada típica poderia ser
+5 100 200 300 400 500
+onde o 5 indica que os cinco valores subsequentes devem
+ser somados.*/
