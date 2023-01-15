184903754f0786d47446c56718aeeae188fe01fb
dehusch <denis.husch@gmail.com>
2023-01-15 20:07:36 -0300

    Solved Cap04 Exerc 9 How to Program book

--- howToprogram/chapter004/exerc004_9.c
+++ howToprogram/chapter004/exerc004_9.c
@@ -1,9 +1,32 @@
-/*Soma de uma sequência de inteiros. Escreva um
-programa que some uma sequência de inteiros. Considere
+/*Soma de uma sequência de inteiros.
+
+Escreva um programa que some uma sequência de inteiros. Considere
 que o primeiro inteiro lido com scanf especifique o
 número de valores restantes a serem inseridos. Seu programa
 deve ler apenas um valor toda vez que scanf for
 executado. Uma sequência de entrada típica poderia ser
 5 100 200 300 400 500
 onde o 5 indica que os cinco valores subsequentes devem
-ser somados.*/
+ser somados.
+*/
+#include <stdio.h>
+
+int main(int argc, char const *argv[])
+{
+    int x, y, z;
+
+    printf("Digite quantos numeros serão inserido: ");
+    scanf("%d", &z);
+
+    for (x = 0; x < z; x++)
+    {
+        printf("Digite um numero: ");
+        scanf("%d", &y);
+
+        y = y + y;
+    }
+
+    printf("%d", y);
+
+    return 0;
+}
