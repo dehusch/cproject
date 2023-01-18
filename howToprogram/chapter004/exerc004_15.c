b05e1cff3ae62de5e859a98115606494975f6392
dehusch <denis.husch@gmail.com>
2023-01-18 01:00:49 -0300

    Solved Cap04 Exerc 15 How to Program book

---
+++ howToprogram/chapter004/exerc004_15.c
@@ -0,0 +1,7 @@
+/*Fatoriais. A função fatorial é usada com frequência
+nos problemas de probabilidade. O fatorial de um inteiro
+positivo n (escrito como n! e pronunciado como ‘fatorial
+de n’) é igual ao produto dos inteiros positivos de 1 a n.
+Escreva um programa que avalie os fatoriais dos inteiros
+de 1 a 5. Imprima os resultados em formato tabular. Que
+dificuldade poderia impedi-lo de calcular o fatorial de 20? */
