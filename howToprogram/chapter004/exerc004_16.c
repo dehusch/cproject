19af123b3fd256a29ad5c2b83c6e41af5dd18959
dehusch <denis.husch@gmail.com>
2023-01-21 10:08:18 -0300

    Solved Cap04 Exerc 16 How to Program book

--- howToprogram/chapter004/exerc004_16.c
+++ howToprogram/chapter004/exerc004_16.c
@@ -6,3 +6,41 @@ uma única instrução printf na forma printf(“*”);
 (isso faz com que os asteriscos sejam impressos lado a
 lado). [Dica: os dois últimos padrões exigem que cada
 linha comece com um número de espaços apropriado.]*/
+#include <stdio.h>
+
+int main(int argc, char const *argv[])
+{
+
+    int linha;
+    int coluna;
+    int espaco;
+
+    for (linha = 1; linha <= 10; linha++)
+    {
+        for (coluna = 1; coluna <= linha; coluna++)
+        {
+            printf("*");
+        }
+        printf("\n");
+    }
+    printf("\n");
+
+    for (linha = 10; linha >= 1; linha--)
+    {
+        for (espaco = 1; espaco <= 10 - linha; espaco++)
+        {
+
+            printf(" ");
+        }
+
+        for (coluna = 1; coluna <= linha; coluna++)
+        {
+            printf("*");
+            printf("\n");
+        }
+
+        printf("\n");
+
+        return 0;
+    }
+}
