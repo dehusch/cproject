00b4a07525297757b4e35ebda0215dab07e57a8d
dehusch <eberhard.husch@outlook.com>
2023-12-21 17:26:59 +0100

    Update Basic

--- howToprogram/chapter001/basic_Overview/basic.c
+++ howToprogram/chapter001/basic_Overview/basic.c
@@ -15,8 +15,9 @@ int main(){
     float High = 1.63;
     float value1=0, value2=0;
 
-    
-    scanf("%d\n", &a);
+    printf("Enter a value: \n");
+    scanf("%d", &a);
+    printf("Enter another value: \n");
     scanf("%d", &b);
     
     c = a % b;
@@ -24,24 +25,24 @@ int main(){
     printf("Division rest = %d\n", c);
     printf("Result of division = %d\n", d);
     printf("Age: %d\n", age);
-    printf("Salary: %f\n", salary);
-    printf("Discount (%%): %f\n", percent_disc);
+    printf("Salary: %.3f\n", salary);
+    printf("Discount (%%): %.2f\n", percent_disc);
     printf("Gender: %c\n", gender);
     printf("High: %.3f\n", High);
     
     
     //float of values 1 and 2
     printf("Enter a value: \n");
-    scanf("%f", &valor1);
+    scanf("%f", &value1);
     printf("Enter another value: \n");
-    scanf("%f", &valor2);
-    printf("The first value ist: %.2f\nThe second value is: %.2f\n", valor1, valor2);
+    scanf("%f", &value2);
+    printf("The first value ist: %.2f\nThe second value is: %.2f\n", value1, value2);
 
     //Constantes
     const float G = 9.80;
 
     printf("\n PI: %.2f", PI); //Constant defined in the header
-    printf("\n G: %.2f\n", G);
+    printf("\n G: %.2f\n", G); //Constant G defined in the main
 
     return 0;
 
