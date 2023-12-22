7060494e43320fa16cdb1d23da6842ee17fa5601
dehusch <eberhard.husch@outlook.com>
2023-12-22 12:49:17 +0100

    Exerc04_17

---
+++ howToprogram/chapter004/exerc004_17.c
@@ -0,0 +1,32 @@
+/*4.17 Calculando limites de crédito. Poupar dinheiro
+vem se tornando algo cada vez mais difícil de se fazer
+durante períodos de recessão, de modo que as empresas
+podem estreitar seus limites de crédito para impedir que
+suas contas a receber (dinheiro devido a elas) se tornem
+muito grandes. Em resposta a uma recessão prolongada,
+uma empresa cortou os limites de crédito de seus
+clientes pela metade. Assim, se um cliente em particular
+tinha um limite de crédito de R$ 2.000,00, agora
+ele é de R$ 1.000,00. Se um cliente tinha um limite de
+R$ 5.000,00, agora ele é de R$ 2.500,00. Escreva um
+programa que analise o status de crédito de três clientes
+dessa empresa. Você receberá as seguintes informações:
+
+a) O número de conta do cliente.
+b) O limite de crédito do cliente antes da recessão.
+c) O saldo atual do cliente (ou seja, o valor que o cliente
+deve à empresa).
+
+Seu programa deve calcular e imprimir o novo limite de
+crédito para cada cliente e deve determinar (e imprimir)
+quais clientes têm saldo atual superior a seus novos limites
+de crédito.
+
+*/
+
+/*Calculate a Customer Credit
+The actual Credit for a customer is $2000,00 and
+over the new recession the actual Credit for each Customer
+will be half!*/
+
+#include <stdio.h>
