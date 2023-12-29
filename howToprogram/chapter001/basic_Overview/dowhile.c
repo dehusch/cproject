40d8ab8395bb716b2f3535c2310bcd91ff79fc50
dehusch <eberhard.husch@outlook.com>
2023-12-29 23:00:23 +0100

    dowhile

--- howToprogram/chapter001/basic_Overview/dowhile.c
+++ howToprogram/chapter001/basic_Overview/dowhile.c
@@ -4,14 +4,16 @@ int main()
 {
      int i = 0;
 
-     while (i != 0) #EXECUTA E DEPOIS VERIFICA
+     while (i != 0) //EXECUTA E DEPOIS VERIFICA
      {
          printf("Teste");
      }
+     printf("i = %d\n", i);
 
-     do {   #VERIFICA E DEPOIS EXECUTA
+     do {  //VERIFICA E DEPOIS EXECUTA
         printf("Teste2");
-     } while (i !=0); #ENQUANTO A CONDICAO NAO MUDA, ELE PARA
+     } while (i !=0); //ENQUANTO A CONDICAO NAO MUDA, ELE PARA
 
+    printf("i = %d\n", i);
     return 0;
 }
