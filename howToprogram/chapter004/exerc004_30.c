2184ae48e29017caf17a0336e059f59998c2ed20
deck <eberhard.husch@outlook.com>
2024-06-04 17:56:08 +0200

    update logical

---
+++ howToprogram/chapter004/exerc004_30.c
@@ -0,0 +1,15 @@
+/*
+ * 4.304.31Substituindo switch por if...else. Reescreva o pro-
+grama da Figura 4.7 substituindo a estrutura switch pela
+estrutura if...else aninhada; tenha o cuidado de tra-
+tar do caso default corretamente. Depois, reescreva essa
+nova versão substituindo a estrutura if...else aninha-
+da por uma série de estruturas if; aqui, também tenha
+o cuidado de lidar com o caso default corretamente
+(isso é mais difícil do que na versão do if...else
+aninhado). Esse exercício demonstra que switch é
+uma conveniência, e que qualquer estrutura switch
+pode ser escrita apenas com instruções de seleção única.
+*/
+
+
